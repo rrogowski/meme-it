c9eef1aa834e9cd4697fe0e37b91d53d73bef9a6
Roman Rogowski <rrogowski@aurorasolar.com>
2023-07-12 22:57:51 -0400

    chore: restore index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+import { subscribe } from "./client/store.js";
 import { render } from "./client/ui.js";
-import { dispatch, subscribe } from "./client/store.js";
 import { App } from "./client/views/app.js";
-import { SelectImage } from "./client/views/select-image.js";
 
-// subscribe(() => render(App));
+subscribe(() => render(App));
 
-subscribe(() => render(SelectImage));
-const payload = { name: "1", uploader: "1" };
-dispatch({ type: "DEBUG", payload });
+// subscribe(() => render(SelectImage));
+// const payload = { name: "1", uploader: "1" };
+// dispatch({ type: "DEBUG", payload });
